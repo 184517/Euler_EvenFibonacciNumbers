20afa3d30abdd5b96e540b782f6982888d12a211
184517 <47399915+184517@users.noreply.github.com>
2019-06-11 09:44:02 -0400

    Add files via upload

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,54 @@
+﻿/* Jordan Ross
+ * June 11, 2019
+ * Sum of all even fibonacci numbers from 1 to 4 million
+ */
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Euler_EvenFibonacciNumbers
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        ulong fourmill = 4000000;
+        ulong start0 = 0;
+        ulong start1 = 1;
+        ulong nextNum;
+        ulong sum;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnGenerate_Click(object sender, RoutedEventArgs e)
+        {
+            for(ulong i = 2; nextNum <= fourmill; i++)
+            {
+                nextNum = start0 + start1;
+                start0 = start1;
+                
+                if(nextNum % 2 == 0)
+                {
+                    sum += nextNum;
+                    
+                }
+                start1 = nextNum;
+            }
+            MessageBox.Show(sum.ToString());
+        }
+    }
+}
